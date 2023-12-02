e670aeca7638acdfdf919fedecaa08759f96fd41
Vaibhavi Lokegaonkar <53178543+Vaibhavi1707@users.noreply.github.com>
2023-12-02 16:38:50 +0000

    Added m2 committed

--- src/ssbdplus/m2.py
+++ src/ssbdplus/m2.py
@@ -1,135 +1,162 @@
-import torchvision.models as models
-import torch.nn as nn
-import torch
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+import os
+
+from tensorflow import keras
+from tensorflow.python.ops.array_ops import concat_v2_eager_fallback
 
 
-"""
-    @class M2: Used to detect the type of stimming behaviour.
-        @component Resnet (or equivalent) base CNN model
-        @component Bi-directional LSTM
-        @component Multihead attention model
-        @component Fully-connected NN
+IMAGE_DIMS = (256, 256)
 
-        @param dropout_rate: The dropout rate used by the model
-            @note Used by the LSTM block as-is, but 0,25 * dropout_rate is used for the Attention block
-            @note A fraction of dropout_rate can be applied to the Fully-connected NN block by the user
-        @param dim_embedding: The embedding dimension that pertains to the LSTM block
-        @param dim_hidden: The hidden dimension pertaining to the LSTM block
-        @param num_lstm_layers: The number of bidirectional LSTM cells
-        @param dim_fc_layer_1: The dimension of the first hidden layer 
-        @param dim_fc_layer_2: The dimension of the second hidden layer
-        @param base_model: The base CNN feature extractor model of the combined model
-            @default torchvision.models.resnet18(pretrained = True)
-            @note If another model is used, the user must ensure that its last layer should output a vector of length dim_embedding
-        @param n_frames: The number of frames of the input video
-            @default 40
-        @param use_movenet: Flag to select if movenet vectors are considered for extracting the model's output
-            @default True
-        @param n_classes: Array containing the respective convolutional stride values
-            @default 3
-"""
-class SSBDModel2(nn.Module):
-    def __init__(self, dropout_rate, dim_embedding, dim_hidden, num_lstm_layers,
-                 dim_fc_layer_1, dim_fc_layer_2,
-                 base_model = models.resnet18(pretrained = True),
-                 n_frames = 40,
-                 use_movenet = True,
-                 n_classes = 3):
-        super(SSBDModel2, self).__init__()
-        
-        self.n_classes = n_classes
-        self.dropout_rate = dropout_rate
-        self.dim_embedding = dim_embedding
-        self.dim_hidden = dim_hidden
-        self.num_lstm_layers = num_lstm_layers
-        self.dim_fc_layer_1 = dim_fc_layer_1
-        self.dim_fc_layer_2 = dim_fc_layer_2
-        self.base_model = base_model
-        self.n_frames = n_frames
-        self.use_movenet = use_movenet
 
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        
-        """
-            @block Resnet architecture (base model)
-        """
-        self.base_model.fc = nn.Linear(
-            self.base_model.fc.in_features, 
-            self.dim_embedding
-        )
-        self.base_model.fc.requires_grad = True
-        
-        """
-            @block Bidirectional LSTM
-        """
-        self.lstm_layer = nn.LSTM(
-            self.dim_embedding,
-            self.dim_hidden,
-            num_layers = self.num_lstm_layers,
-            dropout = self.dropout_rate,
-            bidirectional = True
-        )
-        
-        """
-            @block Multi-headed Attention
-        """
-        self.multihead_attn = nn.MultiheadAttention(
-            embed_dim = self.dim_embedding,
-            dropout = self.dropout_rate/(4),
-            num_heads = 1)
+def get_movenet_data(path):
+  cap = cv.VideoCapture(path)
+  if not cap.isOpened():
+    print("Error")
+    return []
 
-        
-        """
-            @block Fully-connected NN
-        """
-        self.fc = nn.Sequential(
-            nn.Linear(
-                self.dim_hidden,
-                self.dim_fc_layer_1
-            ),
-            nn.LeakyReLU(),
-            nn.Linear(
-                self.dim_fc_layer_1,
-                self.dim_fc_layer_2
-            ),
-            nn.LeakyReLU(),
-            nn.BatchNorm1d(num_features = self.dim_fc_layer_2),
-            nn.Linear(
-                self.dim_fc_layer_2,
-                self.n_classes
-            ),
-            nn.Softmax(dim = 0)
-        )
-        
-    def forward(self, x, hidden = None):
-        vid, movenet_x = x
-        vid = vid.to(self.device)
-        movenet_x = movenet_x.to(self.device, dtype=torch.float)
-        out = None 
-        hidden = None 
-        
-        for t in range(self.n_frames):
-            effective_frames = None
-            with torch.no_grad():
-                frame_features = self.base_model(vid[:, t, :, :, :])
-                effective_frames = frame_features
+  frames = []
+  vid_keypts = []
 
-            if self.use_movenet == True:
-                appended = []
-                for i, feature in enumerate(frame_features):
-                    features = torch.cat([frame_features[i], movenet_x[i, t,:]])
-                    appended.append(features)
-                appended = torch.stack(appended)
-                appended = appended.to(self.device)
-                effective_frames = appended
+  while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+      img1 = frame.copy()
+      img1 = tf.image.resize_with_pad(np.expand_dims(img1, axis=0), 512, 682)
+      frames.append(np.squeeze(img1))
+      
+      # reshape image
+      img = frame.copy()
+      img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 256, 256)
+      input_image = tf.cast(img, dtype=tf.float32)
 
-            out, hidden = self.lstm_layer(
-                effective_frames.unsqueeze(0),
-                hidden
-            )
-            
-        
-        attn_output, attn_output_weights = self.multihead_attn(out[ : , : , :self.dim_embedding], hidden[0], hidden[1])
-        out = attn_output
-            
-        return self.fc(out[-1, :, :])
+      # Input/Output
+      input_details = estimator.get_input_details()
+      output_details = estimator.get_output_details()
+
+      # predict
+      estimator.set_tensor(input_details[0]['index'], np.array(input_image))
+      estimator.invoke()
+
+      keypts = estimator.get_tensor(output_details[0]['index'])
+      vid_keypts.append(keypts)
+
+    else:
+      break
+
+  cap.release()
+  return vid_keypts, frames
+
+
+
+def frame_with_max_change(keypts):
+  max_diff = 0
+  max_loc = 0
+  for frame in range(len(keypts) - 1):
+    shaped1 = np.squeeze(keypts[frame])
+    shaped2 = np.squeeze(keypts[frame + 1])
+
+    diff_mag = np.linalg.norm(shaped2 - shaped1)
+
+    if max_diff < diff_mag:
+      max_diff = diff_mag
+      max_loc = frame
+
+  return max_diff, max_loc
+
+
+
+def processed(keypts):
+  proc_keypts = []
+
+  for keypt in keypts:
+    keypt = np.squeeze(keypt)[:, :2].flatten()
+    # print(keypt.shape)
+    proc_keypts.append(keypt)
+
+  return proc_keypts
+
+
+
+def get_best_frames(video_dir, test = False):
+  best_frames = []
+  set_label = np.array([1.0, 0.0, 0.0], dtype = np.float32)
+  if 'headbanging' in video_dir and not test:
+    set_label = np.array([0.0, 1.0, 0.0], dtype = np.float32)
+  elif 'spinning' in video_dir and not test:
+    set_label = np.array([0.0, 0.0, 1.0], dtype = np.float32)
+  y = []
+  all_keypts = []
+  for video in os.listdir(video_dir):
+    if 'noclass' in video:
+      continue
+
+    keypts, frames = get_movenet_data(video_dir + '/' + video)
+    all_keypts.append(processed(keypts))
+
+    max_diff, max_loc = frame_with_max_change(keypts)
+    best_frames.append(frames[max_loc + 1])
+    if not test:
+      y.append(set_label)
+
+    else:
+      if 'armflapping' in video:
+        y.append(np.array([1.0, 0.0, 0.0], dtype = np.float32))
+      elif 'headbanging' in video:
+        y.append(np.array([0.0, 1.0, 0.0], dtype = np.float32))
+      elif 'spinning' in video:
+        y.append(np.array([0.0, 0.0, 1.0], dtype = np.float32))
+
+  return (best_frames, all_keypts), y
+
+
+
+def create_model():
+  base_model = tf.keras.applications.Xception(
+    weights = 'imagenet',
+    input_shape = (512, 682, 3),
+    include_top = False,
+    pooling = 'avg'
+  )
+
+  base_model.trainable = False
+
+  frame_input = tf.keras.Input(shape = (512, 682, 3))
+  joint_input = tf.keras.Input(shape = (40, 34,))
+
+  x = base_model(frame_input, training = False)
+  y = tf.keras.layers.LSTM(units = 4)(joint_input)
+  combined = tf.keras.layers.Concatenate()([x, y])
+
+  z = tf.keras.layers.UnitNormalization()(combined)
+  z = tf.keras.layers.GaussianDropout(rate = 0.5, seed = 42)(z)
+  z = tf.keras.layers.Dense(3)(z)
+  z = tf.keras.layers.Softmax()(z)
+
+  return tf.keras.Model(inputs = [frame_input, joint_input], outputs = z)
+
+
+
+def train_model(model):
+  model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.1, amsgrad = True),
+              loss = tf.keras.losses.CategoricalCrossentropy(from_logits = False),
+              metrics = [tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+
+  mcp_save = tf.keras.callbacks.ModelCheckpoint('mdl_wts.hdf5', save_best_only=True, monitor='val_categorical_accuracy', mode='max')
+
+  model.fit(x = [train_frames, train_keypts], y = train_y,
+            epochs = 100, validation_data = ([test_frames, test_keypts], test_y),
+            callbacks=[mcp_save], batch_size = 64, shuffle = True)
+
+
+
+def load_model(model_pth):
+  return tf.keras.models.load_model(model_pth)
+
+
+
+def identify_action(model, test_frames, test_keypts):
+  return model.predict([test_frames, test_keypts])
+
